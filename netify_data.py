6d15d83410c9b98fc8c0a92a0bcb9fc124e87bd3
CalebC <calebchuck@berkeley.edu>
2016-04-20 14:57:10 -0700

    version of netify_data

--- netify_data.py
+++ netify_data.py
@@ -1,8 +1,10 @@
 import tensorflow as tf
 from Net.tensor import net6
 import cv2
+import numpy as np
+import os
 
-def netify_image(net, model_path, layer, image):
+def netify_image(net, model_path, layer, image, name):
 	sess = net.load(var_path = model_path)
 	im = inputdata.im2tensor(image, channels = 3)
 
@@ -11,3 +13,15 @@ def netify_image(net, model_path, layer, image):
 
 	with sess.as_default():
 		result = sess.run(layer, feed_dict={net.x:im})
+		np.savetxt(name, result)
+	sess.close()
+
+def generate_rollout_values(net, model_path, layer, rollout_paths, destination):
+	for folder in rollout_paths:
+		for rollout_folder in os.listdir(folder):
+			for image in os.listdir(rollout_folder):
+				print rollout_folder
+				netify_image(net, model_path, layer, image, destination + "/" rollout_folder + "_" + image + ".m")
+
+
+generate_rollout_values(net6, "./../data/net6_04-08-2016_14h46m42s.ckpt", net6.h_fc1, "./../data/traindata", "./../data/featurizedImages/")
