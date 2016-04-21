2d4eb0f7575691dc6dbf769dc030e4fce2ecd845
Caleb Chuck <caleb_chuck@yahoo.com>
2016-04-21 23:14:02 +0000

    netify data updates

--- netify_data.py
+++ netify_data.py
@@ -1,15 +1,22 @@
+import sys
+sys.path.append('/usr/local/lib/python2.7/dist-packages')
+
 import tensorflow as tf
-from Net.tensor import net6
+from Net.tensor import net6, inputdata
 import cv2
 import numpy as np
 import os
 
+
 def netify_image(net, model_path, layer, image, name):
 	sess = net.load(var_path = model_path)
-	im = inputdata.im2tensor(image, channels = 3)
+	print image
+	img = cv2.imread(image)
+	print img
+	im = inputdata.im2tensor(img, channels = 3)
 
-	imshape = image.shape
-	im = np.reshape(image, (-1, imshape[0], imshape[1], imshape[2]))
+	imshape = im.shape
+	im = np.reshape(im, (-1, imshape[0], imshape[1], imshape[2]))
 
 	with sess.as_default():
 		result = sess.run(layer, feed_dict={net.x:im})
@@ -17,11 +24,10 @@ def netify_image(net, model_path, layer, image, name):
 	sess.close()
 
 def generate_rollout_values(net, model_path, layer, rollout_paths, destination):
-	for folder in rollout_paths:
-		for rollout_folder in os.listdir(folder):
-			for image in os.listdir(rollout_folder):
-				print rollout_folder
-				netify_image(net, model_path, layer, image, destination + "/" rollout_folder + "_" + image + ".m")
+	for folder in os.listdir(rollout_paths):
+		for image in os.listdir(rollout_paths + "/" + folder):
+			print image
+			netify_image(net, model_path, layer, rollout_paths+ "/" + folder + "/" + image, destination + "/"+ folder + "_" + image + ".m")
 
-
-generate_rollout_values(net6, "./../data/net6_04-08-2016_14h46m42s.ckpt", net6.h_fc1, "./../data/traindata", "./../data/featurizedImages/")
+net =  net6.NetSix()
+generate_rollout_values(net, "./../data/net6_04-08-2016_14h46m42s.ckpt", net.h_fc1, "./../data/traindata", "./../data/featurizedImages/")
